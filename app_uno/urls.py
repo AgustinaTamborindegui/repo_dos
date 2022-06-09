064b9188722e31d701a96556a7de78e6cc7a5e10
Agustina Tamborindegui <agustinailent@gmail.com>
2022-06-09 15:38:23 -0300

    segundo commit

--- app_uno/urls.py
+++ app_uno/urls.py
@@ -7,9 +7,9 @@ urlpatterns = [
     path("lecturas", views.lecturas, name="lecturas"),
     path("peliculas", views.peliculas, name="peliculas"),
     path("post", views.post, name="post"),
-    path("alta_lecturas", views.alta_lecturas),
-    path("alta_peliculas", views.alta_peliculas),
-    path("alta_post", views.alta_post),
+    path("alta_lecturas", views.alta_lecturas, name="alta_lecturas"),
+    path("alta_peliculas", views.alta_peliculas, name="alta_peliculas"),
+    path("alta_post", views.alta_post, name="alta_post"),
     path("buscar_libro", views.buscar_libro ),
     path("buscar", views.buscar)
 
